30f72072e64bf0a60a8d8144d23d2aaf8b1ed458
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-04-23 19:36:19 -0700

    update main tests

--- ecs_scheduler/main.py
+++ ecs_scheduler/main.py
@@ -27,18 +27,17 @@ def main():
         component_name = config.get('component_name')
         _logger.info('ECS Scheduler v%s', __version__)
 
-        scheduler = ecs_scheduler.scheduld.create(config)
-        server = ecs_scheduler.webapi.create(config, queue)
-
         # NOTE: Flask in debug mode will restart the process
         # so only launch scheduler in the initial startup to avoid duplicate daemons
         # see: https://github.com/pallets/werkzeug/blob/master/werkzeug/_reloader.py
         if not os.getenv('WERKZEUG_RUN_MAIN'):
             _logger.info('Starting scheduld...')
+            scheduler = ecs_scheduler.scheduld.create(config)
             scheduler.start()
 
         _logger.info('Starting webapi...')
         is_debug = config['webapi']['debug']
+        server = ecs_scheduler.webapi.create(config, queue)
         server.run(debug=is_debug, host=None if is_debug else '0.0.0.0', use_evalex=False)
     except Exception:
         _logger.critical('unhandled scheduler exception', exc_info=True)
--- test/test_main.py
+++ test/test_main.py
@@ -1,45 +1,48 @@
 import unittest
 import logging
+import os
 from unittest.mock import patch
+
 from ecs_scheduler.main import main
 
 
-@patch('ecs_scheduler.scheduld.app.run')
-@patch('ecs_scheduler.webapi.server.run')
+@patch('ecs_scheduler.scheduld.create')
+@patch('ecs_scheduler.webapi.create')
 @patch('ecs_scheduler.main.jobtasks.SqsTaskQueue')
 @patch('ecs_scheduler.main.init')
 class MainTests(unittest.TestCase):
-    def test_run_webapi(self, fake_init, fake_queue_class, fake_server_run, fake_app_run):
-        fake_init.config.return_value = {'aws': 'foo', 'component_name': 'webapi'}
+    def test_starts_app(self, fake_init, fake_queue_class, create_webapi, create_scheduld):
+        fake_init.config.return_value = {'aws': 'foo', 'webapi': {'debug': False}}
 
         main()
 
         fake_init.env.assert_called_with()
         fake_queue_class.assert_called_with('foo')
-        fake_server_run.assert_called_with(fake_init.config.return_value, fake_queue_class.return_value)
-        fake_app_run.assert_not_called()
+        create_scheduld.assert_called_with(fake_init.config.return_value)
+        create_webapi.assert_called_with(fake_init.config.return_value, fake_queue_class.return_value)
+        create_scheduld.return_value.start.assert_called_with()
+        create_webapi.return_value.run.assert_called_with(debug=False, host='0.0.0.0', use_evalex=False)
 
-    def test_run_scheduld(self, fake_init, fake_queue_class, fake_server_run, fake_app_run):
-        fake_init.config.return_value = {'aws': 'foo', 'component_name': 'scheduld'}
+    def test_starts_app_with_debug(self, fake_init, fake_queue_class, create_webapi, create_scheduld):
+        fake_init.config.return_value = {'aws': 'foo', 'webapi': {'debug': True}}
 
         main()
 
-        fake_init.env.assert_called_with()
-        fake_queue_class.assert_called_with('foo')
-        fake_app_run.assert_called_with(fake_init.config.return_value, fake_queue_class.return_value)
-        fake_server_run.assert_not_called()
+        create_webapi.return_value.run.assert_called_with(debug=True, host=None, use_evalex=False)
+
+    @patch.dict(os.environ, {'WERKZEUG_RUN_MAIN': 'true'})
+    def test_does_not_start_scheduld_if_child_flask_process(self, fake_init, fake_queue_class, create_webapi, create_scheduld):
+        fake_init.config.return_value = {'aws': 'foo', 'webapi': {'debug': False}}
+
+        main()
+
+        create_scheduld.assert_not_called()
+        create_webapi.assert_called_with(fake_init.config.return_value, fake_queue_class.return_value)
+        create_scheduld.return_value.start.assert_not_called()
+        create_webapi.return_value.run.assert_called_with(debug=False, host='0.0.0.0', use_evalex=False)
 
     @patch.object(logging.getLogger('ecs_scheduler.main'), 'critical')
-    def test_run_raises_exception_with_unknown_component(self, fake_log, fake_init, fake_queue_class, fake_server_run, fake_app_run):
-        fake_init.config.return_value = {'aws': 'foo', 'component_name': 'who knows'}
-
-        with self.assertRaises(RuntimeError):
-            main()
-
-        fake_log.assert_called()
-
-    @patch.object(logging.getLogger('ecs_scheduler.main'), 'critical')
-    def test_run_raises_exceptions(self, fake_log, fake_init, fake_queue_class, fake_server_run, fake_app_run):
+    def test_run_raises_exceptions(self, fake_log, fake_init, fake_queue_class, create_webapi, create_scheduld):
         fake_init.config.side_effect = Exception
 
         with self.assertRaises(Exception):
