afee4647b9b5cada97dedfd72b397e0878b3817d
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-05-03 22:00:34 -0700

    define direct ops queue

--- ecs_scheduler/jobtasks.py
+++ ecs_scheduler/jobtasks.py
@@ -1,4 +1,4 @@
-"""Classes for operating on job tasks"""
+"""Classes for operating on job tasks."""
 import logging
 
 import boto3
@@ -7,6 +7,37 @@ from .serialization import JobOperationSchema
 
 
 _logger = logging.getLogger(__name__)
+
+
+class DirectQueue:
+    """
+    An operations queue directly wired to the scheduler daemon.
+    """
+    def __init__(self):
+        """
+        Create a notifier queue.
+        """
+        self._consumer = None
+
+    def register(self, consumer):
+        """
+        Register a consumer for the operations queue.
+
+        Only supports a single consumer at a time; the existing consumer will be
+        overridden by the new one when this method is called.
+
+        :consumer: An instance of a queue consumer, implementing a notify(job_op) method
+        """
+        self._consumer = consumer
+
+    def post(self, job_op):
+        """
+        Post a job operation to operations queue.
+
+        :param job_op: The job operation to post
+        """
+        if self._consumer:
+            self._consumer.notify(job_op)
 
 
 class SqsTaskQueue:
--- test/test_jobtasks.py
+++ test/test_jobtasks.py
@@ -3,9 +3,28 @@ from unittest.mock import patch, Mock
 
 import marshmallow.exceptions
 
-from ecs_scheduler.jobtasks import SqsTaskQueue, MsgTask, InvalidMessageException
+from ecs_scheduler.jobtasks import DirectQueue, SqsTaskQueue, MsgTask, InvalidMessageException
 from ecs_scheduler.models import JobOperation
 from ecs_scheduler.serialization import JobOperationSchema
+
+
+class DirectQueueTests(unittest.TestCase):
+    def setUp(self):
+        self._target = DirectQueue()
+
+    def test_does_nothing_if_no_consumer(self):
+        try:
+            self._target.post('foo')
+        except Exception:
+            self.fail('post should not have raised an error')
+
+    def test_sends_message_to_consumer(self):
+        consumer = Mock()
+
+        self._target.register(consumer)
+        self._target.post('foo')
+
+        consumer.notify.assert_called_with('foo')
 
 
 class SqsTaskQueueTests(unittest.TestCase):
