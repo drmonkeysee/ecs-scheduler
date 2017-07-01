e21fc4cc1cc87baefbbf4929922947a52f3e0dd9
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-06-30 22:06:16 -0700

    use abc for jobsource classes

--- ecs_scheduler/persistence.py
+++ ecs_scheduler/persistence.py
@@ -1,5 +1,33 @@
 """Built-in job data source implementations."""
-class NullSource:
+from abc import ABCMeta, abstractmethod
+
+
+# TODO: use ABCMeta to allow alternate registrations and pick implementation automatically
+class JobSource(metaclass=ABCMeta):
+    """
+    Abstract base class for Job Source implementations.
+
+    Register a new job source implementation either by inheriting from JobStore
+    or calling JobSource.register(MyJobSourceClass).
+    """
+    @abstractmethod
+    def load_all(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def create(self, job_id, **job):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update(self, job_id, **job):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def delete(self, job_id):
+        raise NotImplementedError()
+
+
+class NullSource(JobSource):
     """
     Null data source.
 
@@ -19,46 +47,16 @@ class NullSource:
         pass
 
 
-class FileSource:
+class FileSource(JobSource):
     """File system data source."""
-    def load_all(self):
-        pass
-
-    def create(self, job_id, **job):
-        pass
-
-    def update(self, job_id, **job):
-        pass
-
-    def delete(self, job_id):
-        pass
+    pass
 
 
-class S3Source:
+class S3Source(JobSource):
     """AWS S3 data source."""
-    def load_all(self):
-        pass
-
-    def create(self, job_id, **job):
-        pass
-
-    def update(self, job_id, **job):
-        pass
-
-    def delete(self, job_id):
-        pass
+    pass
 
 
-class ElasticsearchSource:
+class ElasticsearchSource(JobSource):
     """Elasticsearch data source."""
-    def load_all(self):
-        pass
-
-    def create(self, job_id, **job):
-        pass
-
-    def update(self, job_id, **job):
-        pass
-
-    def delete(self, job_id):
-        pass
+    pass
