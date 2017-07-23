64ea4080d6dd74ba6e0086cbc2aec893889c4983
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-07-22 20:15:24 -0700

    implement elasticsearch store

--- ecs_scheduler/persistence.py
+++ ecs_scheduler/persistence.py
@@ -1,4 +1,14 @@
 """Built-in job data store implementations."""
+import logging
+from datetime import datetime
+
+import elasticsearch
+import elasticsearch.helpers
+
+from .configuration import config
+
+
+_logger = logging.getLogger(__name__)
 
 
 def resolve():
@@ -20,10 +30,10 @@ class NullStore:
     def load_all(self):
         yield from {}.items()
 
-    def create(self, job_id, **job):
+    def create(self, job_id, job_data):
         pass
 
-    def update(self, job_id, **job):
+    def update(self, job_id, job_data):
         pass
 
     def delete(self, job_id):
@@ -43,10 +53,10 @@ class FileStore:
     def load_all(self):
         pass
 
-    def create(self, job_id, **job):
+    def create(self, job_id, job_data):
         pass
 
-    def update(self, job_id, **job):
+    def update(self, job_id, job_data):
         pass
 
     def delete(self, job_id):
@@ -60,4 +70,65 @@ class S3Store:
 
 class ElasticsearchStore:
     """Elasticsearch data store."""
-    pass
+    _DOC_TYPE = 'job'
+    _SCROLL_PERIOD = '1m'
+
+    def __init__(self):
+        """Create store."""
+        self._es = elasticsearch.Elasticsearch(**config['elasticsearch']['client'])
+        self._index = config['elasticsearch']['index']
+        if not self._es.indices.exists(self._index):
+            self._create_index()
+
+    def load_all(self):
+        """
+        Get all job data in the index.
+
+        :returns: Generator yielding job data dictionary for each job
+        """
+        _logger.info('Loading jobs from elasticsearch index %s...', self._index)
+        hits = elasticsearch.helpers.scan(client=self._es,
+            index=self._index,
+            doc_type=self._DOC_TYPE,
+            scroll=self._SCROLL_PERIOD)
+        for hit in hits:
+            yield {'id': hit['_id'], **hit['_source']}
+
+    def create(self, job_id, job_data):
+        """
+        Create a new job document.
+
+        :param job_id: Job document id
+        :param job_data: Job document body
+        """
+        self._es.create(index=self._index, doc_type=self._DOC_TYPE, id=job_id, body=job_data)
+
+    def update(self, job_id, job_data):
+        """
+        Update existing job document.
+
+        :param job_id: Job document id
+        :param job_data: Job document body
+        """
+        self._es.update(index=self._index, doc_type=self._DOC_TYPE, id=job_id, body={'doc': job_data}, retry_on_conflict=3)
+
+    def delete(self, job_id):
+        """
+        Delete a job document.
+
+        :param job_id: Job document id
+        """
+        self._es.delete(index=self._index, doc_type=self._DOC_TYPE, id=job_id)
+
+    def _create_index(self):
+        index_name = f'{self._index}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+        index_settings = {
+            'settings': {
+                'number_of_shards': 3
+            },
+            'aliases': {
+                self._index: {}
+            }
+        }
+        _logger.warning('ECS scheduler index not found; creating index "%s" with alias "%s"', index_name, self._index)
+        self._es.indices.create(index=index_name, body=index_settings)
--- test/test_persistence.py
+++ test/test_persistence.py
@@ -1,29 +1,137 @@
 import unittest
+import logging
+from unittest.mock import patch
+from datetime import datetime
 
-from ecs_scheduler.persistence import NullSource, FileSource
+from ecs_scheduler.persistence import NullStore, FileStore, ElasticsearchStore
 
 
-class NullSourceTests(unittest.TestCase):
+class NullStoreTests(unittest.TestCase):
     def setUp(self):
-        self._target = NullSource()
+        self._target = NullStore()
 
     def test_load_all_returns_empty(self):
-        result = self._target.load_all()
+        result = list(self._target.load_all())
 
-        self.assertEqual({}, result)
+        self.assertEqual([], result)
 
     def test_other_ops_do_nothing(self):
         try:
-            self._target.create('id', a=1, b=2)
-            self._target.update('id', a=1, b=2)
+            self._target.create('id', {'a': 1, 'b': 2})
+            self._target.update('id', {'a': 1, 'b': 2})
             self._target.delete('id')
         except Exception as ex:
             self.fail('Unexpected error raised: {}'.format(ex))
 
 
-class FileSourceTests(unittest.TestCase):
-    def setUp(self):
-        self._target = FileSource()
+class FileStoreTests(unittest.TestCase):
+    pass
 
-    def test(self):
-        self._target.get_all()
+
+class ElasticsearchStoreTests(unittest.TestCase):
+    def setUp(self):
+        with patch('elasticsearch.Elasticsearch') as es_cls, \
+                patch.dict('ecs_scheduler.configuration.config',
+                            {'elasticsearch': {'client': {'foo': 'bar'}, 'index': 'test_index'}}):
+            self._target = ElasticsearchStore()
+            self._es = es_cls.return_value
+
+    @patch('elasticsearch.Elasticsearch')
+    @patch.dict('ecs_scheduler.configuration.config', {'elasticsearch': {'client': {'foo': 'bar'}, 'index': 'test_index'}})
+    def test_init_does_not_create_index_if_present(self, es_cls):
+        es = es_cls.return_value
+        es.indices.exists.return_value = True
+
+        target = ElasticsearchStore()
+
+        es.indices.create.assert_not_called()
+
+    @patch('ecs_scheduler.persistence.datetime')
+    @patch('elasticsearch.Elasticsearch')
+    @patch.object(logging.getLogger('ecs_scheduler.persistence'), 'warning')
+    @patch.dict('ecs_scheduler.configuration.config', {'elasticsearch': {'client': {'foo': 'bar'}, 'index': 'test_index'}})
+    def test_init_does_creates_index_if_not_found(self, warning, es_cls, dt):
+        es = es_cls.return_value
+        es.indices.exists.return_value = False
+        dt.now.return_value = datetime(year=2017, month=6, day=14, hour=13, minute=43, second=54)
+
+        target = ElasticsearchStore()
+
+        expected_body = {
+            'settings': {
+                'number_of_shards': 3
+            },
+            'aliases': {
+                'test_index': {}
+            }
+        }
+        es.indices.create.assert_called_with(index='test_index-20170614-134354', body=expected_body)
+        warning.assert_called()
+
+    @patch('elasticsearch.helpers.scan')
+    @patch.object(logging.getLogger('ecs_scheduler.persistence'), 'info')
+    def test_load_all_returns_empty_if_no_documents(self, info, scan):
+        scan.return_value = []
+
+        results = list(self._target.load_all())
+
+        self.assertEqual([], results)
+        info.assert_called()
+        scan.assert_called_with(client=self._es, index='test_index', doc_type='job', scroll='1m')
+
+    @patch('elasticsearch.helpers.scan')
+    @patch.object(logging.getLogger('ecs_scheduler.persistence'), 'info')
+    def test_load_all_returns_all_documents(self, info, scan):
+        scan.return_value = [
+            {
+                '_id': 1,
+                '_source': {'a': 'foo'}
+            },
+            {
+                '_id': 4,
+                '_source': {'a': 'bar'}
+            },
+            {
+                '_id': 8,
+                '_source': {'a': 'baz'}
+            }
+        ]
+
+        results = list(self._target.load_all())
+
+        expected = [
+            {
+                'id': 1,
+                'a': 'foo'
+            },
+            {
+                'id': 4,
+                'a': 'bar'
+            },
+            {
+                'id': 8,
+                'a': 'baz'
+            }
+        ]
+        self.assertCountEqual(expected, results)
+        info.assert_called()
+        scan.assert_called_with(client=self._es, index='test_index', doc_type='job', scroll='1m')
+
+    def test_create(self):
+        data = {'a': 1, 'b': 2}
+
+        self._target.create(12, data)
+
+        self._es.create.assert_called_with(index='test_index', doc_type='job', id=12, body=data)
+
+    def test_update(self):
+        data = {'a': 1, 'b': 2}
+
+        self._target.update(12, data)
+
+        self._es.update.assert_called_with(index='test_index', doc_type='job', id=12, body={'doc': data}, retry_on_conflict=3)
+
+    def test_delete(self):
+        self._target.delete(12)
+
+        self._es.delete.assert_called_with(index='test_index', doc_type='job', id=12)
