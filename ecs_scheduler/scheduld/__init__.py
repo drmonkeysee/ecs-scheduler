c80bfb5545ab8e9bd7b914cdbfdf8ff8e15d06a3
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-07-24 17:04:08 -0700

    remove jobstore reference from scheduler startup

--- ecs_scheduler/scheduld/__init__.py
+++ ecs_scheduler/scheduld/__init__.py
@@ -1,21 +1,18 @@
 """Scheduler daemon subpackage."""
 from .execution import JobExecutor
 from .scheduler import Scheduler
-from .jobstore import JobStore
 
 
-def create(ops_queue, jobs_datacontext):
+def create(ops_queue, datacontext):
     """
     Create the ecs scheduler daemon.
 
     :param ops_queue: The operations queue from which to process job operations
-    :param jobs_datacontext: The jobs data context for loading and saving jobs
+    :param datacontext: The jobs data context for loading and saving jobs
     :returns: An initialized scheduler instance
     """
-    store = JobStore()
-    
     job_exec = JobExecutor()
     
-    sched = Scheduler(store, job_exec)
+    sched = Scheduler(datacontext, job_exec)
     ops_queue.register(sched)
     return sched
--- test/scheduld/test_subpackage.py
+++ test/scheduld/test_subpackage.py
@@ -7,14 +7,13 @@ from ecs_scheduler.scheduld import create
 class RunTests(unittest.TestCase):
     @patch('ecs_scheduler.scheduld.Scheduler')
     @patch('ecs_scheduler.scheduld.JobExecutor')
-    @patch('ecs_scheduler.scheduld.JobStore')
-    def test_create_scheduld(self, fake_store, fake_exec, fake_sched):
+    def test_create_scheduld(self, fake_exec, fake_sched):
         test_queue = Mock()
+        dc = Mock()
         
-        result = create(test_queue)
+        result = create(test_queue, dc)
 
-        fake_store.assert_called_with()
         fake_exec.assert_called_with()
-        fake_sched.assert_called_with(fake_store.return_value, fake_exec.return_value)
+        fake_sched.assert_called_with(dc, fake_exec.return_value)
         test_queue.register.assert_called_with(fake_sched.return_value)
         self.assertIsNotNone(result)
