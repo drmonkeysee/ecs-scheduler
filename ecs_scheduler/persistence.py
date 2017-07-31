f14de382c92f63d36aa29d34d848598c48da78b9
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-07-30 23:02:01 -0700

    implement s3 load all

--- ecs_scheduler/persistence.py
+++ ecs_scheduler/persistence.py
@@ -1,7 +1,12 @@
 """Built-in job data store implementations."""
 import logging
+import os.path
+import collections
+import json
 from datetime import datetime
 
+import boto3
+import botocore.exceptions
 import elasticsearch
 import elasticsearch.helpers
 
@@ -53,7 +58,86 @@ class SQLiteStore:
 
 class S3Store:
     """AWS S3 data store."""
-    pass
+    _JobObject = collections.namedtuple('JobObject', ['summary', 'prefix', 'job_id', 'ext'])
+    _JOB_EXT = '.json'
+
+    def __init__(self, bucket, prefix=None):
+        """Create store."""
+        self._bucket = boto3.resource('s3').Bucket(bucket)
+        self._prefix = prefix or ''
+        self._ensure_bucket()
+
+    def load_all(self):
+        """
+        Get all job objects from the S3 location.
+
+        :returns: Generator yielding job data dictionary for each job
+        """
+        msg = f'Loading jobs from S3 bucket {self._bucket}'
+        if self._prefix:
+            msg += f', prefix {self._prefix}'
+        msg += '...'
+        _logger.info(msg)
+        job_objects = self._get_objects()
+        for jo in job_objects:
+            full_object = jo.summary.get()
+            contents = full_object['Body'].read()
+            job_data = json.loads(contents, encoding='utf-8')
+            job_data.update(id=jo.job_id)
+            yield job_data
+
+    def create(self, job_id, job_data):
+        """
+        Create a new job S3 object.
+
+        :param job_id: Job object id
+        :param job_data: Job object contents
+        """
+        pass
+
+    def update(self, job_id, job_data):
+        """
+        Update existing job object.
+
+        :param job_id: Job object id
+        :param job_data: Job object body
+        """
+        pass
+
+    def delete(self, job_id):
+        """
+        Delete a job object.
+
+        :param job_id: Job object id
+        """
+        pass
+
+    def _ensure_bucket(self):
+        try:
+            self._bucket.load()
+        except botocore.exceptions.ClientError as ex:
+            if ex.response['Error']['Code'] == '404':
+                _logger.warning('S3 bucket not found; creating bucket "%s"', self._bucket)
+                current_region = boto3.session.Session().region_name
+                self._bucket.create(CreateBucketConfiguration={'LocationConstraint': current_region})
+            else:
+                raise
+
+    def _get_objects(self):
+        objects = self._bucket.objects.filter(Prefix=self._prefix)
+        job_objects = (self._make_job_object(o) for o in objects)
+        return (j for j in job_objects if self._valid_job(j))
+
+    def _make_job_object(self, s3_obj_summary):
+        prefix, name = os.path.split(s3_obj_summary.key)
+        job_id, extension = os.path.splitext(name)
+        return self._JobObject(s3_obj_summary, prefix, job_id, extension)
+
+    def _valid_job(self, job_object):
+        prefix_check = self._prefix[:-1] \
+                        if self._prefix and self._prefix[-1] == '/' \
+                        else self._prefix
+        return job_object.prefix == prefix_check and job_object.ext == self._JOB_EXT
 
 
 class ElasticsearchStore:
@@ -65,8 +149,7 @@ class ElasticsearchStore:
         """Create store."""
         self._es = elasticsearch.Elasticsearch(**config['elasticsearch']['client'])
         self._index = config['elasticsearch']['index']
-        if not self._es.indices.exists(self._index):
-            self._create_index()
+        self._ensure_index()
 
     def load_all(self):
         """
@@ -108,7 +191,9 @@ class ElasticsearchStore:
         """
         self._es.delete(index=self._index, doc_type=self._DOC_TYPE, id=job_id)
 
-    def _create_index(self):
+    def _ensure_index(self):
+        if self._es.indices.exists(self._index):
+            return
         index_name = f'{self._index}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
         index_settings = {
             'settings': {
@@ -118,5 +203,5 @@ class ElasticsearchStore:
                 self._index: {}
             }
         }
-        _logger.warning('ECS scheduler index not found; creating index "%s" with alias "%s"', index_name, self._index)
+        _logger.warning('Elasticsearch index not found; creating index "%s" with alias "%s"', index_name, self._index)
         self._es.indices.create(index=index_name, body=index_settings)
--- test/test_persistence.py
+++ test/test_persistence.py
@@ -1,9 +1,12 @@
 import unittest
 import logging
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from datetime import datetime
+from io import BytesIO
 
-from ecs_scheduler.persistence import NullStore, SQLiteStore, ElasticsearchStore
+import botocore.exceptions
+
+from ecs_scheduler.persistence import NullStore, S3Store, SQLiteStore, ElasticsearchStore
 
 
 class NullStoreTests(unittest.TestCase):
@@ -26,6 +29,156 @@ class NullStoreTests(unittest.TestCase):
 
 class SQLiteStoreTests(unittest.TestCase):
     pass
+
+
+class S3StoreTests(unittest.TestCase):
+    def setUp(self):
+        with patch('boto3.resource') as self._s3:
+            self._target = S3Store('test-bucket')
+            self._bucket = self._s3.return_value.Bucket.return_value
+
+    def test_init(self):
+        self._s3.return_value.Bucket.assert_called_with('test-bucket')
+        self._bucket.load.assert_called_with()
+        self._bucket.create.assert_not_called()
+
+    @patch.object(logging.getLogger('ecs_scheduler.persistence'), 'warning')
+    def test_init_creates_bucket_if_not_found(self, warning):
+        with patch('boto3.resource') as s3, \
+                patch('boto3.session.Session') as s:
+            bucket = s3.return_value.Bucket.return_value
+            bucket.load.side_effect = botocore.exceptions.ClientError({'Error': {'Code': '404'}}, 'fake_name')
+            session = s.return_value
+            session.region_name = 'test-region'
+            target = S3Store('test-bucket', 'test-prefix')
+
+        bucket.create.assert_called_with(CreateBucketConfiguration={'LocationConstraint': 'test-region'})
+        warning.assert_called()
+
+    @patch.object(logging.getLogger('ecs_scheduler.persistence'), 'warning')
+    def test_init_raises_unknown_errors(self, warning):
+        with patch('boto3.resource') as s3:
+            bucket = s3.return_value.Bucket.return_value
+            bucket.load.side_effect = botocore.exceptions.ClientError({'Error': {'Code': '500'}}, 'fake_name')
+            with self.assertRaises(botocore.exceptions.ClientError):
+                target = S3Store('test-bucket', 'test-prefix')
+
+        bucket.create.assert_not_called()
+        warning.assert_not_called()
+
+    def test_load_all_yields_nothing_if_empty(self):
+        self._bucket.objects.filter.return_value = []
+
+        results = list(self._target.load_all())
+
+        self.assertEqual([], results)
+        self._bucket.objects.filter.assert_called_with(Prefix='')
+
+    def test_load_all_yields_json_objects_from_root_bucket(self):
+        self._bucket.objects.filter.return_value = [
+            Mock(key='foo.json', get=lambda: {'Body': BytesIO(b'{"a": 1}')}),
+            Mock(key='bar.json', get=lambda: {'Body': BytesIO(b'{"b": 2}')}),
+            Mock(key='baz.json', get=lambda: {'Body': BytesIO(b'{"c": 3}')})
+        ]
+
+        results = list(self._target.load_all())
+
+        expected = [
+            {'id': 'foo', 'a': 1},
+            {'id': 'bar', 'b': 2},
+            {'id': 'baz', 'c': 3}
+        ]
+        self.assertCountEqual(expected, results)
+        self._bucket.objects.filter.assert_called_with(Prefix='')
+
+    def test_load_all_ignores_other_bucket_contents(self):
+        self._bucket.objects.filter.return_value = [
+            Mock(key='foo.json', get=lambda: {'Body': BytesIO(b'{"a": 1}')}),
+            Mock(key='a-prefix/'),
+            Mock(key='bar.json', get=lambda: {'Body': BytesIO(b'{"b": 2}')}),
+            Mock(key='another-prefix/'),
+            Mock(key='baz.json', get=lambda: {'Body': BytesIO(b'{"c": 3}')}),
+            Mock(key='another-prefix/foo.json'),
+            Mock(key='bort.txt'),
+            Mock(key='a-file'),
+        ]
+
+        results = list(self._target.load_all())
+
+        expected = [
+            {'id': 'foo', 'a': 1},
+            {'id': 'bar', 'b': 2},
+            {'id': 'baz', 'c': 3}
+        ]
+        self.assertCountEqual(expected, results)
+        self._bucket.objects.filter.assert_called_with(Prefix='')
+
+    def test_load_all_prefix_yields_nothing_if_empty(self):
+        self._target._prefix = 'test-prefix'
+
+        results = list(self._target.load_all())
+
+        self.assertEqual([], results)
+        self._bucket.objects.filter.assert_called_with(Prefix='test-prefix')
+
+    def test_load_all_prefix_yields_json_objects(self):
+        self._target._prefix = 'test-prefix'
+        self._bucket.objects.filter.return_value = [
+            Mock(key='test-prefix/foo.json', get=lambda: {'Body': BytesIO(b'{"a": 1}')}),
+            Mock(key='test-prefix/bar.json', get=lambda: {'Body': BytesIO(b'{"b": 2}')}),
+            Mock(key='test-prefix/baz.json', get=lambda: {'Body': BytesIO(b'{"c": 3}')})
+        ]
+
+        results = list(self._target.load_all())
+
+        expected = [
+            {'id': 'foo', 'a': 1},
+            {'id': 'bar', 'b': 2},
+            {'id': 'baz', 'c': 3}
+        ]
+        self.assertCountEqual(expected, results)
+        self._bucket.objects.filter.assert_called_with(Prefix='test-prefix')
+
+    def test_load_all_prefix_works_if_prefix_contains_slash(self):
+        self._target._prefix = 'test-prefix/'
+        self._bucket.objects.filter.return_value = [
+            Mock(key='test-prefix/foo.json', get=lambda: {'Body': BytesIO(b'{"a": 1}')}),
+            Mock(key='test-prefix/bar.json', get=lambda: {'Body': BytesIO(b'{"b": 2}')}),
+            Mock(key='test-prefix/baz.json', get=lambda: {'Body': BytesIO(b'{"c": 3}')})
+        ]
+
+        results = list(self._target.load_all())
+
+        expected = [
+            {'id': 'foo', 'a': 1},
+            {'id': 'bar', 'b': 2},
+            {'id': 'baz', 'c': 3}
+        ]
+        self.assertCountEqual(expected, results)
+        self._bucket.objects.filter.assert_called_with(Prefix='test-prefix/')
+
+    def test_load_all_prefix_ignores_subfolders(self):
+        self._target._prefix = 'test-prefix'
+        self._bucket.objects.filter.return_value = [
+            Mock(key='test-prefix/foo.json', get=lambda: {'Body': BytesIO(b'{"a": 1}')}),
+            Mock(key='a-prefix/'),
+            Mock(key='test-prefix/bar.json', get=lambda: {'Body': BytesIO(b'{"b": 2}')}),
+            Mock(key='another-prefix/'),
+            Mock(key='test-prefix/baz.json', get=lambda: {'Body': BytesIO(b'{"c": 3}')}),
+            Mock(key='another-prefix/foo.json'),
+            Mock(key='bort.txt'),
+            Mock(key='a-file'),
+        ]
+
+        results = list(self._target.load_all())
+
+        expected = [
+            {'id': 'foo', 'a': 1},
+            {'id': 'bar', 'b': 2},
+            {'id': 'baz', 'c': 3}
+        ]
+        self.assertCountEqual(expected, results)
+        self._bucket.objects.filter.assert_called_with(Prefix='test-prefix')
 
 
 class ElasticsearchStoreTests(unittest.TestCase):
