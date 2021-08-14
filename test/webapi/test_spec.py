e7793c74bdee5aec9ee2d830847e08654d0b48f0
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-08-13 20:18:28 -0700

    fix flask mocks

--- test/webapi/test_spec.py
+++ test/webapi/test_spec.py
@@ -6,11 +6,11 @@ from ecs_scheduler.webapi.spec import Spec
 
 class SpecTests(unittest.TestCase):
     @patch('flask_swagger.swagger')
-    @patch('flask.current_app')
-    def test_get(self, fake_flask_app, fake_swagger):
+    @patch('ecs_scheduler.webapi.spec.flask')
+    def test_get(self, fake_flask, fake_swagger):
         spec = Spec()
 
         response = spec.get()
 
-        fake_swagger.assert_called_with(fake_flask_app)
+        fake_swagger.assert_called_with(fake_flask.current_app)
         self.assertIs(fake_swagger.return_value, response)
